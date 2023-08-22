52196ea49c54bd6a5489a1080ce14885f75f20b3
Matias Zanan <matiaszanan@gmail.com>
2023-08-22 00:11:25 -0300

    [Form]

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00526D",
+        secondary: "#578C9C",
+        surface: "#FEFEFE",
+        accent: "#CE1300",
+        text: "#2A2A2A",
+        placeholder: "#1E1E1E",
+        shadow: "#002626",
+      },
+    },
+  },
+  plugins: [],
+};
